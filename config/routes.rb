19ed512bff08cb198112174e2057d31bef28a536
hiro-22 <70711048+hiro-22@users.noreply.github.com>
2020-10-27 19:41:37 +0900

    ユーザー管理機能を実装する

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  root to: "items#index"
+  resources :items, only: [:index, :new, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+# ルーティングにlocalhost::3000にアクセスしたときに「itemsコントローラーのindexアクション」が作動する様に設定する
+# itemsコントローラーを作成しましょう。その後にindexアクションを定義してあげましょう
+# 一度サーバーを抜けて再度rails sをしたときにトップページが開いていることを確認しましょう。
